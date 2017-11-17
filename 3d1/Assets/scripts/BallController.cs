b264d971428b9b7013a1c5daeabc29e4f843c7a7
Carey Sanchez <yerafne@gmail.com>
2017-11-17 20:51:16 +0100

    actualizacion

---
+++ 3d1/Assets/scripts/BallController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+	private float speed;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		this.rb = GetComponent<Rigidbody>();
+		this.speed = 5;
+	}
+	
+
+	void FixedUpdate () {
+		float horizontalAxis = Input.GetAxis("Horizontal");
+		float verticalAxis = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(horizontalAxis, 0.0f,verticalAxis);
+		rb.AddForce(movement*speed);
+	}
+}
